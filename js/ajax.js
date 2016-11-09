2e0d215630c22887dfc2a156304191b4ef7680c5
Erozbliz <erozbliz@hotmail.com>
2016-11-09 18:48:08 -0500

    Ajout switch

--- js/ajax.js
+++ js/ajax.js
@@ -7,7 +7,7 @@ $("#btRefresh2").click(function(){
     	},
     	error: function(req, status, ex) {
     		//$(".div1").html("error");
-    		$(".div1").append("error");
+    		$(".div1").append("erreur");
     	 	console.log(req,status,ex);
     	 }
 });
@@ -15,25 +15,46 @@ $("#btRefresh2").click(function(){
 
 
 
-  $("#btRefresh").click(function(){
-        var idmysoluce = "eee";
-        var varmysoluce = "eee"; //indice
-  
-       //A garder
-       $.ajax({
-            url: '../includes/indice_ajax.php',
-            type: 'POST', 
-            data: {
-                dataSoluce: varmysoluce, 
-                dataId: idmysoluce,
-            },
-            success: function(data) {
-                var jsondata=data; 
-                alert (data);
-            },
-            error: function(json) {
-                alert('false');
-                $(".div1").append(" error ");
-            }
-        });
+$("#btRefresh").click(function(){
+    var idmysoluce = "eee";
+    var varmysoluce = "eee"; //indice
+   //A garder
+   $.ajax({
+        url: '../includes/indice_ajax.php',
+        type: 'POST', 
+        data: {
+            dataSoluce: varmysoluce, 
+            dataId: idmysoluce,
+        },
+        success: function(data) {
+            var jsondata=data; 
+            alert (data);
+        },
+        error: function(json) {
+            alert('erreur btRefresh');
+            $(".div1").html(" erreur ");
+        }
     });
+});
+
+
+$("#btAutoRefresh").click(function(){
+    var varmysoluce = "eee";
+   $.ajax({
+        url: '../includes/indice_ajax.php',
+        type: 'POST', 
+        data: {
+            dataSoluce: varmysoluce, 
+        },
+        success: function(data) {
+            var jsondata=data; 
+            alert (data);
+        },
+        error: function(json) {
+            alert('erreur btAutoRefresh');
+            $(".div1").html(" erreur btAutoRefresh");
+        }
+    });
+});
+
+
