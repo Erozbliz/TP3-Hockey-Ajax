70aea38d23ca56e862c32402752f3976a9891e33
Erozbliz <erozbliz@hotmail.com>
2016-11-12 19:29:50 -0500

    actualisation de la liste des matchs ok

--- js/ajax.js
+++ js/ajax.js
@@ -21,9 +21,8 @@ $("#btRefresh").click(function(){
         url: 'http://127.0.0.1:4444/getListMatch',
         type: 'GET',
         success: function(data,callback) {
-           // var jsondata=data; 
-            alert (data);
-            $(".div1").html(callback + " : " + data);
+            $(".div1").html(callback);
+            miseAjourListHtml(data);
         },
         error: function(json) {
             alert('erreur btRefresh');
@@ -33,17 +32,37 @@ $("#btRefresh").click(function(){
 });
 
 
+//Pemet de mettre a jour la liste des matchs dynamiquement
+function miseAjourListHtml(data){
+     //       [0]       0   1           2           3               4           5       6
+    //     num_match:[Name,ButEquipe1,ButEquipe2,PenaltyEquipe1,PenaltyEquipe2,Date,StatusMatch]
+    //Exemple = {"0":["red1 VS blue1",4,1,0,0,"2016\/11\/12 18:26:54","PERIODE 1"]}
+    var jsondata = JSON.parse(data);
+    //alert ("jsondata[0]['0']");
+    alert ("Il y a "+Object.keys(jsondata).length+" matchs");
+    //On efface l'ancien html
+    $("#idListMatch").html("");
+    var str= "";
+    for(var i=0;i<Object.keys(jsondata).length;i++){
+        str +='<li>';
+        str +='<div class="collapsible-header"><i class="material-icons">games</i>Match '+i+' : '+jsondata[i]['0']+'</div>';
+        str +='<div class="collapsible-body">';
+        str +='<p> Résultats : '+jsondata[i]['1']+' / '+jsondata[i]['2']+'<br> Pénalité : '+jsondata[i]['3']+' / '+jsondata[i]['4']+' <br>';
+        str +=' Chrono : '+jsondata[i]['5']+' <br> Status : '+jsondata[i]['6']+'  <br> </p>';
+        str +='</div>';
+        str +='</li>';
+       $("#idListMatch").html(str);
+    }
+}
+
+//Utiliser pour l'autorefresh
 function refrehListMatch() {
-   var varmysoluce = "eee";
    $.ajax({
         url: 'http://127.0.0.1:4444/getListMatch',
-        type: 'POST', 
-        data: {
-            dataSoluce: varmysoluce, 
-        },
-        success: function(data) {
-            var jsondata=data; 
-            alert (data);
+        type: 'GET', 
+        success: function(data,callback) {
+            $(".div1").html(callback);
+            miseAjourListHtml(data);
         },
         error: function(json) {
             alert('erreur btAutoRefresh');
@@ -79,3 +98,34 @@ $("#btAutoRefresh").click(function(){
     }
     console.log(boolSwitch);
 });
+
+
+
+
+
+/**
+//       [0]       0   1           2           3               4           5       6
+//     num_match:[Name,ButEquipe1,ButEquipe2,PenaltyEquipe1,PenaltyEquipe2,Date,StatusMatch]
+//Exemple = {"0":["red1 VS blue1",4,1,0,0,"2016\/11\/12 18:26:54","PERIODE 1"]}
+EXEMPLE JSON
+{
+  "0": [
+    "red1 VS blue1",
+    4,
+    1,
+    0,
+    0,
+    "2016/11/12 18:26:54",
+    "PERIODE 1"
+  ],
+  "1": [
+    "red2 VS blue2",
+    0,
+    1,
+    1,
+    2,
+    "2016/11/12 18:27:26",
+    "PERIODE 1"
+  ]
+}
+**/
